fa1a029798b9703e85a48ca8bd5d8c6a39b48c52
tehwolf <robert.weyres@conciso.de>
2023-11-24 13:13:08 +0100

    Adapt jest config

--- apps/angular-jest-lodash-es/jest.config.ts
+++ apps/angular-jest-lodash-es/jest.config.ts
@@ -13,7 +13,11 @@ export default {
       },
     ],
   },
-  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
+  transformIgnorePatterns: [
+    'node_modules/(?!.*\\.mjs$)',
+    'node_modules/lodash-es/*',
+  ],
+  moduleNameMapper: { '^lodash-es$': 'lodash', '^lodash-es/(.*)': 'lodash/$1' },
   snapshotSerializers: [
     'jest-preset-angular/build/serializers/no-ng-attributes',
     'jest-preset-angular/build/serializers/ng-snapshot',
